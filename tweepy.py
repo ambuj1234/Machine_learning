5df1437a0d51aa60c64ad4ae3a3c1172e144fda9
ambuj1234 <43312023+ambuj1234@users.noreply.github.com>
2019-06-17 15:26:23 +0530

    twitter

---
+++ tweepy.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import tweepy
+import csv
+
+
+# In[2]:
+
+
+c_key="kqEGmgAt3WNava7BNq9loZNNX"
+c_sec="dcELgyinahrFM3GE6HPk1VcTT99GAOzN6HZQGA1apJ1kj4BmjW"
+a_key="276375321-YX0Au6SyRczoe2tiOjchRbhANOMs1bjAsMOr1nmW"
+a_sec="xEoT9B36t9XA8pidcUrGELidVC8VBsewjRN0m8CMJAlq3"
+
+
+
+# In[3]:
+
+
+auth=tweepy.OAuthHandler(c_key,c_sec)
+auth.set_access_token(a_key,a_sec)
+api=tweepy.API(auth)
+
+
+
+# In[4]:
+
+
+x=5 #no of tweets to be extracted
+keyword="cwc"
+csvFile = open('cw.csv', 'a')
+#Use csv Writer
+csvWriter = csv.writer(csvFile)
+
+
+# In[5]:
+
+
+for tweet in tweepy.Cursor(api.search,q=keyword,count=50,lang="en",since="2019-06-14").items():
+    print (tweet.created_at, tweet.text)
+csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
+
+
+# In[ ]:
+
+
+
+
